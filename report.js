cfb029c73d7cb26b9bb8673764d5560c1335ccec
limon27121 <moontaser009@gmail.com>
2024-05-30 13:18:32 +0600

    upload my result

---
+++ report.js
@@ -0,0 +1,18 @@
+
+const newman = require('newman');
+// require('dotenv').config();
+ 
+newman.run({
+    // collection:`https://api.postman.com/collections/1844288-70ede3a9-093f-49ec-9a00-6bc8f9bd2c55?access_key=${process.env.secretKey}`,
+    collection:`https://api.postman.com/collections/24742376-0a57d6d0-2f8d-4aab-9b5f-de7e96f57609?access_key=${process.env.secretKey}`,
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
